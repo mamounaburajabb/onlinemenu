15a0c68fdcc2c94d4bb17094faadd5d2ba22997f
beginner09500 <beginnertopro0950@gmail.com>
2022-12-14 15:53:19 +0200

    last update

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
-
+import Header from '../../component/header/header'
+import './home.css'
 const Home = () => {
-  return (
-    <div>Home</div>
-  )
+    return (
+        <div className='home'>
+            <Header />
+            <div>
+
+            </div>
+            {/* <h1 className='imagess'>
+                  Enjoy Our
+                  Delicious Meal
+              </h1> */}
+
+
+        </div>
+    )
 }
 
 export default Home
