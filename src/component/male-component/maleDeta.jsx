145a342c05882ff8e1808634aea0c1742b55596a
beginner09500 <beginnertopro0950@gmail.com>
2022-12-17 14:06:39 +0200

    last update

--- src/component/male-component/maleDeta.jsx
+++ src/component/male-component/maleDeta.jsx
@@ -13,8 +13,8 @@ const MealDetails = () => {
                         src="https://images.pexels.com/photos/1694865/pexels-photo-1694865.jpeg?auto=compress&cs=tinysrgb&w=600" alt="" />
                 </div>
                 <div className='mealInfo'>
-                    <h1>pizza</h1>
-                    <h6>25$</h6>
+                    <h1>Coffee</h1>
+                    <h6>10$</h6>
                     <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Enim velit quis, beatae maiores est voluptates ipsam dicta nam aspernatur facilis illo, temporibus maxime non ad eaque doloribus eos. Voluptas, rerum.</p>
                     <label htmlFor=""></label>
                     <input type="checkbox" name="" id="" />
